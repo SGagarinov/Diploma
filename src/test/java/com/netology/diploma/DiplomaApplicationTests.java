1c66d966f9e15eeb39af3da0c8273fcafb56c36a
Abriial <codjumper@mail.ru>
2023-06-23 22:43:41 +0300

    Remove test

--- src/test/java/com/netology/diploma/DiplomaApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.netology.diploma;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DiplomaApplicationTests {
-
-}
